0e5c16061f6ab101c5b6fc677acc09d4d278cd99
suryab27 <boppudisurya27@gmail.com>
2024-07-03 16:57:42 -0500

    fibonacci

---
+++ CoreJava_Programs/src/programs/fibonacci.java
@@ -0,0 +1,23 @@
+package programs;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+			 int n1=0,n2=1,n3,i,N=10;    
+			 System.out.print(n1+" "+n2);  
+			    
+			 for(i=2;i<N;++i) 
+			 {    
+			  n3=n1+n2;    
+			  System.out.print(" "+n3);    
+			 n1=n2;    
+			  n2=n3;    
+			 }    
+			  
+			}
+			
+
+
+	}
+
+
