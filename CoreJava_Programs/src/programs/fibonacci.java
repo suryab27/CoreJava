8d334df37a60e326739ba06b3a3f7cc697b3c48b
suryab27 <boppudisurya27@gmail.com>
2024-07-04 13:09:22 -0500

    modified

--- CoreJava_Programs/src/programs/fibonacci.java
+++ CoreJava_Programs/src/programs/fibonacci.java
@@ -3,21 +3,13 @@ package programs;
 public class fibonacci {
 
 	public static void main(String[] args) {
-			 int n1=0,n2=1,n3,i,N=10;    
-			 System.out.print(n1+" "+n2);  
-			    
-			 for(i=2;i<N;++i) 
-			 {    
-			  n3=n1+n2;    
-			  System.out.print(" "+n3);    
-			 n1=n2;    
-			  n2=n3;    
-			 }    
-			  
-			}
-			
-
-
+		int n1 = 0, n2 = 1, n3, i, last = 25;
+		System.out.print(n1 + " " + n2);
+		for (i = 2; i < last; ++i) {
+			n3 = n1 + n2;
+			System.out.print(" " + n3);
+			n1 = n2;
+			n2 = n3;
+		}
 	}
-
-
+}
